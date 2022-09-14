6c6a903446d1edcf067707d8ac70dcc868049ceb
sandy0518 <sai.sandeep.906@gmail.com>
2022-09-14 13:14:01 +0530

    project added

---
+++ src/test/java/practice/Prog1.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the 1st program");
+	}
+
+}
---
+++ src/test/java/practice/Prog2.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Prog2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is 2nd program");
+	}
+
+}
